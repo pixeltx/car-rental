8ed94b5d33222647f938585fa948156062614689
pixeltx <arvinradithya.0@gmail.com>
2025-01-09 09:37:19 +0700

    added car model+migrate

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/order', function () {
+    return view('order');
+})->middleware(['auth', 'verified'])->name('order');
+
 require __DIR__.'/auth.php';
